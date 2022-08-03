c52881d7a6dd5adbdf550ec53ed19b2c9a318900
Joseph Cauchi <jcauchi@novibet.com>
2022-08-03 16:32:47 +0200

    Add to wuery with SAS

--- AzureBlobStoragePOC/Program.cs
+++ AzureBlobStoragePOC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Azure.Storage.Blobs;
+using AzureBlobStoragePOCConsole;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.IO;
@@ -10,22 +11,41 @@ namespace AzureBlobStoragePOC
 	{
 		static async Task Main(string[] args)
 		{
-			string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=novicd;AccountKey=HN3/VCYYfjZprGYSL7zoiyM4unusN+O+wQ4AWdeRDDDi9CD7GThAtzq6uFtRTadqn2IiZR9P2tco+ASttx/lNw==;EndpointSuffix=core.windows.net";
-			string storageContainerName = "services-customerdocuments";
-
-			string blobFilename = "test1";
-
-
-			var filePath = @"C:\development\POC\BlobStoragePOC\BlobStoragePOC\AzureBlobStoragePOC\image1.png";
+			// common 
+			var filePath = @"C:\development\POC\BlobStoragePOC\BlobStoragePOC\AzureBlobStoragePOC\Capture.PNG";
 			byte[] file = File.ReadAllBytes(filePath);
 			Stream stream = new MemoryStream(file);
 
-			var upload = await Upload.UploadAsync(stream, blobFilename, storageConnectionString, storageContainerName);
+
+			// using DefaultEndpointsProtocol connection string (Key1 or Key2) - This has full access to also delete
+			//string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=novicd;AccountKey=CkY1PNN0ND5rLbdxg30SHev/gnJymqmqCYtucHJ+R81nVIaWyQzPFlgV9qDOfHD6R+r7M7pc0dnN+AStiJuA9w==;EndpointSuffix=core.windows.net";
+			//string storageContainerName = "services-customerdocuments";
+			//string blobFilename = "test2";
+
+			//var upload = await Upload.UploadAsync(stream, blobFilename, storageConnectionString, storageContainerName);
+			//var download = await Download.DownloadAsync(blobFilename, storageConnectionString, storageContainerName);
+
+			// ---------------------------------------------------------------------------------------
+			// ---------------------------------------------------------------------------------------
+			// ---------------------------------------------------------------------------------------
+			// ---------------------------------------------------------------------------------------
+
+			// SAS - gives you more control of the access to give
+			string blobSasUrl = "https://novicd.blob.core.windows.net/services-customerdocuments?sp=rcl&st=2022-08-03T10:07:31Z&se=2023-08-03T18:07:31Z&spr=https&sv=2021-06-08&sr=c&sig=zPF%2BbrsIRlP1oWhPQ3B6yvlLS%2BmO2%2BBg0KaeTjg5DVY%3D";
+			string blobName = "Trial1";
+
+			var sasUpload = await SASImplementation.UploadAsync(blobSasUrl, blobName, stream);
+			var sasDownloadContent = await SASImplementation.DownloadAsync(blobSasUrl, blobName);
+			// save to disk
+			File.WriteAllBytes("TaghnaLiGibnaLura.png", sasDownloadContent.Value.Content.ToArray());
+
+			var delete = await SASImplementation.Delete(blobSasUrl, blobName);
 
 
-			var download = await Download.DownloadAsync(blobFilename, storageConnectionString, storageContainerName);
 
-			
+
+
+
 			Console.ReadLine();
 		}
 	}
---
+++ AzureBlobStoragePOC/SASImplementation.cs
@@ -0,0 +1,105 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureBlobStoragePOCConsole
+{
+	static class SASImplementation
+	{
+		public static async Task<bool> UploadAsync(string blobSasUrl, string blobName, Stream stream)
+		{
+			try
+			{
+				Uri uri = new Uri(blobSasUrl);
+				BlobContainerClient container = new BlobContainerClient(uri);
+								
+				//Return a reference to a blob to be created in the container.
+				BlobClient blob = container.GetBlobClient(blobName);
+				// Open a stream for the file we want to upload
+				await using (Stream? data = stream)//.OpenReadStream())
+				{
+					// Upload the file async
+					var r = await blob.UploadAsync(data);
+				}
+
+				//blob.Upload(BinaryData.FromStream(stream));
+
+				return true;
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+			
+		}
+
+
+		public static async Task<Response<BlobDownloadResult>> DownloadAsync(string blobSasUrl, string blobName)
+		{
+			try
+			{
+				Uri uri = new Uri(blobSasUrl);
+				BlobContainerClient container = new BlobContainerClient(uri);
+
+				//Return a reference to a blob to be created in the container.
+				BlobClient blob = container.GetBlobClient(blobName);
+
+				//BlobDownloadInfo download = blob.Download();
+				bool fileExists = await blob.ExistsAsync();
+				if (fileExists)
+				{
+					var data = await blob.OpenReadAsync();
+					Stream blobContent = data;
+
+					// Download the file details async
+					Response<BlobDownloadResult> contentResult = await blob.DownloadContentAsync();
+
+					// Add data to variables in order to return a BlobDto
+					string name = blobName;
+					string contentType = contentResult.Value.Details.ContentType;
+
+					return contentResult;
+				}
+
+				return null;
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+
+		}
+
+
+		public static async Task<bool> Delete(string blobSasUrl, string blobName)
+		{
+			try
+			{
+				Uri uri = new Uri(blobSasUrl);
+				BlobContainerClient container = new BlobContainerClient(uri);
+
+				//Return a reference to a blob to be created in the container.
+				BlobClient blob = container.GetBlobClient(blobName);
+
+				blob.Delete();
+
+				return true;
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+
+		}
+
+	}
+}
